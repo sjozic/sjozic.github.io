4fdc519eb45ee03a36f0d04fadd4b4b28b5163d6
sjozic <47329980+sjozic@users.noreply.github.com>
2019-02-25 11:00:11 -0600

    reee

--- primitive shapes/sketch.js
+++ primitive shapes/sketch.js
@@ -7,25 +7,37 @@
 
 let randomMode = false;
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+
+
 function draw() {
+
+  //top left info box
   rectMode(CORNER);
   fill(255);
   rect(10, 10, 150, 55);
+
+  //random mode indicator
   if (randomMode === true) {
     fill(0, 255, 0);
     rect(110, 20, 35, 35);
   }
 }
 
+
+
 function keyPressed() {
   rectMode(CENTER);
   ellipseMode(CENTER);
 
   fill(random(0,255), random(0, 255), random(0,255));
+  
+  
+  //ellipses
   if (key === "s") {
     if (randomMode === false){
       ellipse(mouseX, mouseY, random(20, 100), random(20, 100));  
@@ -34,6 +46,9 @@ function keyPressed() {
       ellipse(random(0, windowWidth), random(0, windowHeight), random(20, 100), random(20, 100));
     }
   }
+
+
+  //rectangles
   if (key === "a") {
     if (randomMode === false) {
       rect(mouseX, mouseY, random(20, 90), random(20, 90));
@@ -42,17 +57,26 @@ function keyPressed() {
       rect(random(0, windowWidth), random(0, windowHeight), random(20, 90), random(20, 90));
     }
   }
+
+
+  //triangles
   if (key === "d") {
     if (randomMode === false) {
-      triangle(random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
+      triangle(mouseX, mouseY, random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
     }
     else {
       triangle(random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
     }
   }
+
+
+  //clear
   if (key === "q") {
     background(255);
   }
+
+
+  //random mode switch
   if (key === "e") {
     randomMode = !randomMode;
   }
--- variable shenanigans/sketch.js
+++ variable shenanigans/sketch.js
@@ -5,11 +5,71 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let buttonX, buttonY, buttonSize;
+let rectX, rectY, rectW, rectH, rectShade;
+let fadeIn = false;
+let buttonOver = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  //ellipse variables
+  buttonX = width/2;
+  buttonY = height/4;
+  buttonSize = 50;
+  ellipseMode(CENTER);
+
+  //rect variables
+  rectX = width/2;
+  rectY = height*0.6;
+  rectW = width/2;
+  rectH = height*0.4;
+  rectShade = 0;
+  noStroke();
+  rectMode(CENTER);
 }
 
+
+function mouseDistance(x1, y1, x2, y2){
+  let a = abs(x1 - x2);
+  let b = abs(y1 - y2);
+  let c = sqrt(sq(a) + sq(b));
+  return c;
+}
+
+
+function mousePressed(){
+  if (buttonOver){
+    fadeIn = !fadeIn;
+  }
+}
+
+
 function draw() {
-  background(220);
+  background(255);
+  print (mouseDistance(mouseX, mouseY, buttonX, buttonY));
+
+
+  //draw ellipse
+  if (mouseDistance(mouseX,mouseY,buttonX,buttonY)<buttonSize/2){
+    fill(200, 40, 50);
+    buttonOver = true;
+  }
+  else {
+    fill(0, 0, 0);
+    buttonOver = false;
+  }
+  ellipse(buttonX, buttonY, buttonSize, buttonSize);
+
+
+  //draw rectangle
+  if (fadeIn === true){
+    rectShade -= 2;
+    fill(rectShade);
+  }
+  else {
+    rectShade += 2;
+    fill(rectShade);
+    rect(rectX, rectY, rectW, rectH);
+  }
 }
