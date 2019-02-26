cf4d0622eb0beb55a06159885f2ea36b5c5ae8e9
sjozic <47329980+sjozic@users.noreply.github.com>
2019-02-26 10:34:22 -0600

    FINISHED PRIMITIVE

--- primitive shapes/sketch.js
+++ primitive shapes/sketch.js
@@ -18,18 +18,32 @@ function draw() {
 
   //top left info box
   rectMode(CORNER);
+  textFont("Arial");
   fill(255);
   rect(10, 10, 150, 55);
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  textSize(25);
+  text("Simon J", 15, 42.5);
 
   //random mode indicator
   if (randomMode === true) {
+    
+    fill(random(0, 255), random(0, 255), random(0, 255));
+    triangle(random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
+    rect(random(0, windowWidth), random(0, windowHeight), random(20, 90), random(20, 90));
+    ellipse(random(0, windowWidth), random(0, windowHeight), random(20, 100), random(20, 100));
+    
+    
     fill(0, 255, 0);
     rect(110, 20, 35, 35);
+    fill(0);
+    textSize(9);
+    text("Random", 110, 40);
   }
 }
 
 
-
+//shapes by key press
 function keyPressed() {
   rectMode(CENTER);
   ellipseMode(CENTER);
@@ -42,9 +56,6 @@ function keyPressed() {
     if (randomMode === false){
       ellipse(mouseX, mouseY, random(20, 100), random(20, 100));  
     }
-    else {
-      ellipse(random(0, windowWidth), random(0, windowHeight), random(20, 100), random(20, 100));
-    }
   }
 
 
@@ -52,9 +63,6 @@ function keyPressed() {
   if (key === "a") {
     if (randomMode === false) {
       rect(mouseX, mouseY, random(20, 90), random(20, 90));
-    }
-    else {
-      rect(random(0, windowWidth), random(0, windowHeight), random(20, 90), random(20, 90));
     }
   }
 
@@ -64,14 +72,11 @@ function keyPressed() {
     if (randomMode === false) {
       triangle(mouseX, mouseY, random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
     }
-    else {
-      triangle(random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
-    }
   }
 
 
   //clear
-  if (key === "q") {
+  if (keyCode === 32) {
     background(255);
   }
 
