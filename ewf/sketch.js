dda4b533318bcc95c730eb70b3454ae5e9ce9f4c
sjozic <47329980+sjozic@users.noreply.github.com>
2019-03-05 11:02:07 -0600

    dom shenanigans

--- ewf/sketch.js
+++ ewf/sketch.js
@@ -5,20 +5,56 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let fillMain = (0, 0, 0);
-let slider;
+let fillA, fillB, fillC;
+let sizeA, sizeB;
+let radio;
+let picker;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  fillA = createSlider(0, 255, 100);
+  fillA.position(10, 10);
+  fillA.style('width', '80px');
+
+  fillB = createSlider(0, 255, 100);
+  fillB.position(10, 50);
+  fillB.style('width', '80px');
+
+  fillC = createSlider(0, 255, 100);
+  fillC.position(10, 90);
+  fillC.style('width', '80px');
+
+
+  sizeA = createSlider(5, 255, 100);
+  sizeA.position(100, 10);
+  sizeA.style('width', '80px');
+
+  sizeB = createSlider(5, 255, 100);
+  sizeB.position(100, 50);
+  sizeB.style('width', '80px');
+
+  radio = createRadio();
+  radio.option('square');
+  radio.option('ellipse');
+  radio.position(190, 10);
+  print(radio.value());
+
+  picker = createColorPicker();
+  picker.position(10, 125)
 }
 
 function draw() {
-  background(220);
+  background(0);
 
   rectMode(CENTER);
 
-  fill(fillMain);
-  rect(width/2, height-height/4, 200, 100);
-}
+  
+  fill(fillA.value(), fillB.value(), fillC.value());
 
+  if (radio.value() === 'square'){
+    rect(width/2, height-height/4, sizeA.value(), sizeB.value());
+  }
+  else {
+    ellipse(width/2, height-height/4, sizeA.value(), sizeB.value());
+  }
+}
