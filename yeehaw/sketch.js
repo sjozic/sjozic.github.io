49b8fc47b6d93344d6b70f78efa6e30a3f38852b
sjozic <47329980+sjozic@users.noreply.github.com>
2019-03-15 11:02:22 -0600

    dijerydoo

--- yeehaw/sketch.js
+++ yeehaw/sketch.js
@@ -6,52 +6,61 @@
 // - describe what you did to take this project "above and beyond"
 
 let start = 0;
-let inc = 0.002;
+let inc = 0.02;
+let scrolling = true;
+let incChanger;
+let speedMult;
+let flagY = 0;
+let flagx = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  generateTerrain();
   noStroke();
+
+  incChanger = createSlider(1, 300, 0.01);
+  incChanger.position(10, 10);
+  incChanger.style('width', '100px');
+
+  speedMult = createSlider(1, 50, 0.01);
+  speedMult.position(10, 50);
+  speedMult.style('width', '100px');
+
+
+  
 }
 
 function draw() {
+
+  inc = incChanger.value() / 1000;
+
   background(51);
-  stroke(255);
-  noFill();
   beginShape();
   let xOff = start;
   let x = 0;
   for (let x = 0; x <= width; x++) {
-    stroke(255);
+    stroke(noise(xOff)*255, noise(xOff), noise(xOff));
     let y = noise(xOff) * height;
-    vertex(x, y);
-  
-    xOff += inc;
-  }
-  endShape();
-  
-
-  start += inc;
-}
-
-
-function generateTerrain() {
-
-  background(51);
-  stroke(255);
-  noFill();
-  beginShape();
-  let xOff = start;
-  let x = 0;
-  for (let x = 0; x <= width; x++) {
-    stroke(255);
-    let y = noise(xOff) * height;
-    vertex(x, y);
+    rectMode(CORNER);
+    rect(x, y, 1, height);
 
     xOff += inc;
   }
   endShape();
 
-  start += inc;
-}
+
+  if (scrolling === true){
+    start += inc*speedMult.value();
+  }
+  else if(scrolling === false) {
+    //
+  }
+
+
   
+}
+
+function keyPressed(){
+  if (key === 's') {
+    scrolling = !scrolling;
+  }
+}
