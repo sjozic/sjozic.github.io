4fa693216ed73a5bc112de55f0a922e1d6cadede
sjozic <47329980+sjozic@users.noreply.github.com>
2019-03-20 10:57:50 -0600

    sprite anim time

--- lion images/sketch.js
+++ lion images/sketch.js
@@ -10,25 +10,49 @@ let direction = 1;
   //1 = left
   //2 = right
 let pinImages = [];
+let pngNum = 0;
+let pinNum = 0;
+let speed;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 
+
 function preload() {
   lionL = loadImage('assets/lion-left.png');
   lionR = loadImage('assets/lion-right.png');
-  pinImages.push(loadImage('assets/pin-00.png'));
+  for (let i = 0; i < 9; i ++){
+    pinImages.push(loadImage('assets/pin-0' + pngNum + '.png'));
+    pngNum ++;
+  }
 }
+
+
 
 function draw() {
   background(220);
-  image(pinImages[0], width/2, height/2);
-
+  pinwheel();
   moving();
   imageMode(CENTER);
+  lions();
+}
 
+
+
+function moving(){
+  if (mouseX > pmouseX){
+    direction = 2;
+  }
+  else if (mouseX < pmouseX){
+    direction = 1;
+  }
+}
+
+
+
+function lions(){
   if (direction === 1){
     image(lionL, mouseX, mouseY, 200, 150);
   }
@@ -37,11 +61,15 @@ function draw() {
   }
 }
 
-function moving(){
-  if (mouseX > pmouseX){
-    direction = 2;
-  }
-  else if (mouseX < pmouseX){
-    direction = 1;
+
+
+function pinwheel(){
+  image(pinImages[pinNum], width/2, height/2);
+  speed = map (mouseX, 0, width, 2, 8);
+  if(frameCount % int(speed) === 0){
+    pinNum ++;
+    if (pinNum > 8){
+      pinNum = 0;
+    }
   }
 }
