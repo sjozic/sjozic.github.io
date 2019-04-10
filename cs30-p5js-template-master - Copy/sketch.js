28bdd1daa13da1f41cc110d959923ab9ac2debf9
sjozic <47329980+sjozic@users.noreply.github.com>
2019-04-10 10:58:39 -0600

    coolor

--- cs30-p5js-template-master - Copy/sketch.js
+++ cs30-p5js-template-master - Copy/sketch.js
@@ -2,48 +2,47 @@
 // Your Name
 // Date
 //
-// Extra for Experts:
+// Extra for Experts:1
 // - describe what you did to take this project "above and beyond"
 
-let mountHeightChange = 125;
-let start = 0;
-let inc = 0.02;
+
+let rectWidth = 5;
+let rectHeight = 25;
+let colors = ["#D3BE7C", "#CDA185", "#C8848E", "#C26796", "#BC4A9F"];
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noLoop();
+}
+
+function drawRowCustom(yPos) {
+  colorMode(RGB);
+  let counter = 1;
+  for (let x = 0; x < width; x += rectWidth) {
+    fill(colors[int(random(colors.length))]);
+    rect(x, yPos, rectWidth, rectHeight);
+    counter ++;
+  }
+}
+
+function drawRowRGB(yPos) {
+  colorMode(RGB);
+  for (let x = 0; x < width; x += rectWidth) {
+    fill(random(255), random(255), random(255));
+    rect(x, yPos, rectWidth, rectHeight);
+  }
+}
+
+function drawRowHSB(yPos) {
+  colorMode(HSB);
+  for (let x = 0; x < width; x += rectWidth) {
+    fill(x % 360, 255, 255);
+    rect(x, yPos, rectWidth, rectHeight);
+  }
 }
 
 function draw() {
-  sky();
-  mountains();
-  grass();
-}
-
-
-
-function sky(){
-  fill(135, 206, 235);
-  rect(0, 0, width, height);  
-}
-
-
-
-function mountains(){
-  beginShape();
-
-  let xOff = start;
-  for (let x = 0; x <= width; x++) {
-    stroke(100);
-    let y = noise(xOff) * height - mountHeightChange;
-    rectMode(CORNER);
-    rect(x, y, 1, height);
-    xOff += inc;
-  }
-  endShape();
-}
-
-function grass(){
-  ellipse(0, )
+  drawRowRGB(height * 0.2);
+  drawRowHSB(height * 0.5);
+  drawRowCustom(height *0.8);
 }
